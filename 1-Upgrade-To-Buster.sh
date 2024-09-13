56dc5192f2b0e9787c4f8ef0e49b5433a771d834
meokgo <cimdieke@hotmail.com>
2024-09-13 08:39:37 -0500

    1-Upgrade-To-Buster.sh

--- 1-Upgrade-To-Buster.sh
+++ 1-Upgrade-To-Buster.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
   rm /etc/apt/sources.list /etc/apt/sources.list.d/nodejs.list /etc/apt/sources.list.d/security.list /etc/apt/sources.list.d/ubnt-unifi.list
-  apt-get -y --purge autoremove unifi freeradius
