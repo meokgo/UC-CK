9c2b0b37376b2e9826b8e6151f53743630e35c20
meokgo <cimdieke@hotmail.com>
2024-09-15 11:39:15 -0500

    2-Upgrade-To-Bullseye.sh

--- 2-Upgrade-To-Bullseye.sh
+++ 2-Upgrade-To-Bullseye.sh
@@ -60,7 +60,7 @@ echo "****Install full Bullseye upgrade****" | sed  -e :a -e "s/^.\{1,$(tput col
   echo $(date)":" '\033[0;36m'"\033[1m****Full upgrade complete****\033[0m" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
 #Remove un-needed packages.
 echo "****Cleanup****" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
-  apt update && apt -y --purge autoremove ubnt-archive-keyring ubnt-crash-report ubnt-unifi-setup bt-proxy cloudkey-webui && apt -y --purge autoremove
+  apt update && apt -y --purge autoremove ubnt-archive-keyring ubnt-crash-report ubnt-unifi-setup bt-proxy cloudkey-webui && apt -y --purge autoremove && rm -R /etc/bt-proxy
 #Fix network settings.
   update-alternatives --set iptables /usr/sbin/iptables-legacy
 echo $(date)":" '\033[0;32m'"\033[1mRebooting in 5 seconds...\033[0m" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
