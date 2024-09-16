1b95450e1dda9de474324e2b0e7d171a92ad8d63
meokgo <cimdieke@hotmail.com>
2024-09-16 16:28:58 -0500

    Install-Tools.sh

--- Install-Tools.sh
+++ Install-Tools.sh
@@ -1,3 +1,24 @@
+#!/bin/sh
+#This script will install some useful tools
+#Download script: wget https://raw.githubusercontent.com/meokgo/UC-CK/main/Install-Tools.sh
+#Make script executable: chmod +x Install-Tools.sh
+#Run script: ./Install-Tools.sh
+#Check if script is run as root
+if ! [ $(id -u) = 0 ]; then
+  echo '\033[0;31m'"\033[1mMust run script as root.\033[0m"
+  exit 1
+fi
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mInstall tools? (y/n)\033[0m")" yn
+  case $yn in
+    [yY]) echo '\033[0;36m'"\033[1mProceeding with install.\033[0m"
+      break;;
+    [nN]) echo '\033[0;35m'"\033[1mExiting...\033[0m";
+      exit;;
+    *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";
+  esac
+done
+echo $(date)":" '\033[0;36m'"\033[1mStarting install...\033[0m"
 #Install tools
   curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null && curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list && apt update && apt -y install fzf tldr cmatrix iperf3 speedtest-cli stress s-tui nnn ncdu links2 telnet tailscale
 #Enable device as a subnet router
