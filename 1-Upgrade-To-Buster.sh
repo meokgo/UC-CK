b165bc5c5252eb1716be7f1946eaf7eb14337b2f
meokgo <cimdieke@hotmail.com>
2024-09-16 14:36:05 -0500

    1-Upgrade-To-Buster.sh

--- 1-Upgrade-To-Buster.sh
+++ 1-Upgrade-To-Buster.sh
@@ -63,9 +63,9 @@ echo '\033[0;36m'"\033[1mInitial upgrade to Buster...\033[0m"
   apt-get -y clean
   apt update
   DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-  echo $(date)":" '\033[0;36m'"\033[1Initial upgrade complete.\033[0m"
+  echo $(date)":" '\033[0;36m'"\033[1mInitial upgrade complete.\033[0m"
   sleep 2
-echo '\033[0;36m'"\033[1Install full Buster upgrade...\033[0m"
+echo '\033[0;36m'"\033[1mInstall full Buster upgrade...\033[0m"
   apt update
   DEBIAN_FRONTEND=noninteractive apt -y full-upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
   apt -y autoremove
