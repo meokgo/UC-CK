d943bbad0f7149fe0c6b65cb6eccc5e81e69d7fa
meokgo <cimdieke@hotmail.com>
2024-09-16 13:15:26 -0500

    1-Upgrade-To-Buster.sh

--- 1-Upgrade-To-Buster.sh
+++ 1-Upgrade-To-Buster.sh
@@ -11,19 +11,21 @@ if ! [ $(id -u) = 0 ]; then
   echo '\033[0;31m'"\033[1mMust run script as root.\033[0m"
   exit 1
 fi
-read -p "$(echo '\033[0;106m'"\033[30mUpgrade Cloud Key OS to Buster? (y/n)\033[0m")" yn
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mUpgrade Cloud Key OS to Buster? (y/n)\033[0m")" yn
   case $yn in
-    [yY]) echo '\033[0;36m'"\033[1mProceeding with upgrade.\033[0m";;
+    [yY]) echo '\033[0;36m'"\033[1mProceeding with upgrade.\033[0m"
+      break;;
     [nN]) echo '\033[0;35m'"\033[1mExiting...\033[0m";
       exit;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";
-      exit 1;;
   esac
+done
 echo $(date)":" '\033[0;36m'"\033[1mStarting upgrade...\033[0m"
 #Check for valid OS version
 echo '\033[0;36m'"\033[1mChecking OS version...\033[0m"
   OS_Version=$(lsb_release -a | grep Codename)
-    echo '\033[0;36m'"\033[1mCurrent OS $OS_Version\033[0m"
+  echo '\033[0;36m'"\033[1mCurrent OS $OS_Version\033[0m"
   case $OS_Version in
     *"jessie") echo '\033[0;36m'"\033[1mValid OS.\033[0m";;
     *) echo '\033[0;31m'"\033[1mInvalid OS. Script only upgrades OS from Jessie (Debian 8) to Buster (Debian 10).\033[0m";
@@ -32,7 +34,7 @@ echo '\033[0;36m'"\033[1mChecking OS version...\033[0m"
 #Check for valid kernel version
 echo '\033[0;36m'"\033[1mChecking kernel version...\033[0m"
   Kernel_Version=$(uname -r)
-    echo '\033[0;36m'"\033[1mKernel version: $Kernel_Version\033[0m"
+  echo '\033[0;36m'"\033[1mKernel version: $Kernel_Version\033[0m"
   case $Kernel_Version in
     3.10.20-ubnt-mtk ) echo '\033[0;36m'"\033[1mValid kernel.\033[0m";;
     * ) echo '\033[0;31m'"\033[1mInvalid kernel. Script only works on kernel 3.10.20-ubnt-mtk.\033[0m"
@@ -83,37 +85,50 @@ read -p "$(echo '\033[0;106m'"\033[30mUpdate timezone? (y/n)\033[0m")" yn
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
 #Option to replace motd
-read -p "$(echo '\033[0;106m'"\033[30mReplace motd? (y/n)\033[0m")" yn
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mReplace motd? (y/n)\033[0m")" yn
   case $yn in
-    [yY]) wget -O /etc/motd https://raw.githubusercontent.com/meokgo/UC-CK/main/motd;;
-    [nN]) echo '\033[0;35m'"\033[1mNot replacing motd.\033[0m";;
+    [yY]) wget -O /etc/motd https://raw.githubusercontent.com/meokgo/UC-CK/main/motd
+      break;;
+    [nN]) echo '\033[0;35m'"\033[1mNot replacing motd.\033[0m"
+      break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
+done
 #Option to enable automatic updates
-read -p "$(echo '\033[0;106m'"\033[30mEnable automatic updates? (y/n)\033[0m")" yn
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mEnable automatic updates? (y/n)\033[0m")" yn
   case $yn in
-    [yY]) apt -y install unattended-upgrades && DEBIAN_FRONTEND=noninteractive dpkg-reconfigure --priority=low unattended-upgrades && sed -i 's|//Unattended-Upgrade::Automatic-Reboot "false";|Unattended-Upgrade::Automatic-Reboot "true";|g' /etc/apt/apt.conf.d/50unattended-upgrades && systemctl start unattended-upgrades && systemctl enable unattended-upgrades;;
-    [nN]) echo '\033[0;35m'"\033[1mNot enabling automatic updates.\033[0m";;
+    [yY]) apt -y install unattended-upgrades && DEBIAN_FRONTEND=noninteractive dpkg-reconfigure --priority=low unattended-upgrades && sed -i 's|//Unattended-Upgrade::Automatic-Reboot "false";|Unattended-Upgrade::Automatic-Reboot "true";|g' /etc/apt/apt.conf.d/50unattended-upgrades && systemctl start unattended-upgrades && systemctl enable unattended-upgrades
+      break;;
+    [nN]) echo '\033[0;35m'"\033[1mNot enabling automatic updates.\033[0m"
+      break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
+done
 #Update root password, option to add user
 echo '\033[0;106m'"\033[30mUpdate root user password\033[0m"
 passwd root
-read -p "$(echo '\033[0;106m'"\033[30mAdd new sudo user? (y/n)\033[0m")" yn
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mAdd new sudo user? (y/n)\033[0m")" yn
   case $yn in
     [yY]) read -p "$(echo '\033[0;106m'"\033[30mEnter new user name:\033[0m ")" New_User && 
-        if [ -z "$New_User" ]; then
-          echo '\033[0;35m'"\033[1mNothing entered, not adding new sudo user.\033[0m"
-        else
-          adduser $New_User
-          usermod -aG sudo $New_User
-          echo '\033[0;36m'"\033[1m$New_User added to sudo group.\033[0m"
-        fi;;
-    [nN]) echo '\033[0;35m'"\033[1mNot adding new sudo user.\033[0m";;
+      if [ -z "$New_User" ]; then
+        echo '\033[0;35m'"\033[1mNothing entered, not adding new sudo user.\033[0m"
+      else
+        adduser $New_User
+        usermod -aG sudo $New_User
+        echo '\033[0;36m'"\033[1m$New_User added to sudo group.\033[0m"
+      fi
+      break;;
+    [nN]) echo '\033[0;35m'"\033[1mNot adding new sudo user.\033[0m"
+      break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
+done
 #Option to harden SSH
-read -p "$(echo '\033[0;106m'"\033[30mHarden SSH settings? (y/n)\033[0m")" yn
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mHarden SSH settings? (y/n)\033[0m")" yn
   case $yn in
     [yY]) sed -i 's|LoginGraceTime 120|LoginGraceTime 2m|g' /etc/ssh/sshd_config && sed -i 's|PermitRootLogin yes|PermitRootLogin no|g' /etc/ssh/sshd_config && echo "MaxAuthTries 5" >> /etc/ssh/sshd_config && echo "MaxSessions 1" >> /etc/ssh/sshd_config && echo "AddressFamily inet" >> /etc/ssh/sshd_config && read -p "$(echo '\033[0;106m'"\033[30mEnter new SSH port:\033[0m ")" New_Port && 
       if [ -z "$New_Port" ]; then
@@ -122,16 +137,19 @@ read -p "$(echo '\033[0;106m'"\033[30mHarden SSH settings? (y/n)\033[0m")" yn
         sed -i "s|Port 22|Port $New_Port|g" /etc/ssh/sshd_config
       fi
       /etc/init.d/ssh restart
-      echo '\033[0;36m'"\033[1mSSH settings updated.\033[0m";;
-    [nN]) echo '\033[0;35m'"\033[1mNot hardening SSH settings.\033[0m";;
+      echo '\033[0;36m'"\033[1mSSH settings updated.\033[0m"
+      break;;
+    [nN]) echo '\033[0;35m'"\033[1mNot hardening SSH settings.\033[0m"
+      break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
+done
 echo '\033[0;36m'"\033[1mInstalling ufw and creating firewall rule for SSH...\033[0m"
   apt -y install ufw
   sed -i 's|IPV6=yes|IPV6=no|g' /etc/default/ufw
   SSH_Port=$(cat /etc/ssh/sshd_config | grep Port | sed 's|Port ||g')
   echo '\033[0;36m'"\033[1mCurrent SSH port:\033[0m "$SSH_Port
-  ufw allow 7722/tcp comment 'SSH Port'
+  ufw allow $SSH_Port/tcp comment 'SSH Port'
   ufw status verbose
   ufw enable
 echo $(date)":" '\033[0;32m'"\033[1mRebooting in 5 seconds...\033[0m"
