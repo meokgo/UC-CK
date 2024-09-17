f8d9a31711ab67816b2365f0723ddf6e3d3a9565
meokgo <cimdieke@hotmail.com>
2024-09-17 13:20:30 -0500

    Install-Tools.sh

--- Install-Tools.sh
+++ Install-Tools.sh
@@ -23,7 +23,7 @@ echo $(date)":" '\033[0;36m'"\033[1mStarting install...\033[0m"
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null && curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
 #Install tools
 apt update && apt -y install fzf tldr cmatrix iperf3 speedtest-cli stress s-tui nnn ncdu links2 telnet tailscale
-#Tailscale/Headscale setup
+#Tailscale/Headscale initial setup
 while : ; do
   read -p "$(echo '\033[0;106m'"\033[30mSetup Tailscale/Headscale? (y/n)\033[0m ")" yn
   case $yn in
@@ -37,34 +37,34 @@ while : ; do
           break
         fi
       done;;
-    [nN]) echo '\033[0;35m'"\033[1mSkipping Tailscale/Headscale setup.\033[0m"
+    [nN]) echo '\033[0;35m'"\033[1mSkipping Tailscale/Headscale initial setup.\033[0m"
       break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
 done
 #Tailscale/Headscale enable device as a subnet router
 while : ; do
-  read -p "$(echo '\033[0;106m'"\033[30mEnable device as subnet router? (y/n)\033[0m ")" yn
+  read -p "$(echo '\033[0;106m'"\033[30mEnable device as Tailscale/Headscale subnet router? (y/n)\033[0m ")" yn
   case $yn in
     [yY]) echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.d/99-tailscale.conf && echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.d/99-tailscale.conf && sysctl -p /etc/sysctl.d/99-tailscale.conf
       break;;
-    [nN]) echo '\033[0;35m'"\033[1mNot enabling as subnet router.\033[0m"
+    [nN]) echo '\033[0;35m'"\033[1mNot enabling as Tailscale/Headscale subnet router.\033[0m"
       break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
 done
 #Tailscale/Headscale advertise subnet routes
 while : ; do
-  read -p "$(echo '\033[0;106m'"\033[30mUpdate advertised subnet routes? (y/n)\033[0m ")" yn
+  read -p "$(echo '\033[0;106m'"\033[30mUpdate Tailscale/Headscale advertised subnet routes? (y/n)\033[0m ")" yn
   case $yn in
     [yY]) read -p "$(echo '\033[0;106m'"\033[30mEnter new subnet/s to advertise: (e.g., 192.168.1.0/24 or 192.168.1.0/24,10.1.1.0/24)\033[0m ")" New_Subnet && 
       if [ -z "$New_Subnet" ]; then
-        echo '\033[0;35m'"\033[1mNothing entered, not updating advertised subnet routes.\033[0m"
+        echo '\033[0;35m'"\033[1mNothing entered, not updating Tailscale/Headscale advertised subnet routes.\033[0m"
       else
         tailscale up --advertise-routes=$New_Subnet
       fi
       break;;
-    [nN]) echo '\033[0;35m'"\033[1mNot updating advertised subnet routes.\033[0m"
+    [nN]) echo '\033[0;35m'"\033[1mNot updating Tailscale/Headscale advertised subnet routes.\033[0m"
       break;;
     *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
   esac
