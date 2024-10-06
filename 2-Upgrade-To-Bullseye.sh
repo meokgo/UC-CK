06cf6d16aca4c8ce34b8de5142995e1fadfaef6e
meokgo <cimdieke@hotmail.com>
2024-10-06 11:31:41 -0500

    2-Upgrade-To-Bullseye.sh

--- 2-Upgrade-To-Bullseye.sh
+++ 2-Upgrade-To-Bullseye.sh
@@ -69,6 +69,17 @@ echo '\033[0;36m'"\033[1mCleanup...\033[0m"
   apt update
   DEBIAN_FRONTEND=noninteractive apt -y --purge autoremove -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 echo '\033[0;36m'"\033[1mCleanup complete.\033[0m"
+#Option to change timezone, default is PDT
+while : ; do
+  read -p "$(echo '\033[0;106m'"\033[30mUpdate timezone? (y/n)\033[0m ")" yn
+  case $yn in
+    [yY]) dpkg-reconfigure tzdata
+      break;;
+    [nN]) echo '\033[0;35m'"\033[1mNot updating timezone.\033[0m"
+      break;;
+    *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
+  esac
+done
 #Option to change hostname
 read -p "$(echo '\033[0;106m'"\033[30mNew hostname (leave blank to keep current):\033[0m ")" New_Name
   if [ -z "$New_Name" ]; then
