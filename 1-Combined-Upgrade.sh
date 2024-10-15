6112e8aecfb49313d8a154748428b266b5e5ed8d
meokgo <cimdieke@hotmail.com>
2024-10-15 09:21:21 -0500

    1-Combined-Upgrade.sh

--- 1-Combined-Upgrade.sh
+++ 1-Combined-Upgrade.sh
@@ -150,6 +150,13 @@ deb-src https://deb.debian.org/debian bullseye-updates main contrib non-free" > 
         sed -i "s|0.ubnt.pool.ntp.org ||g" /etc/systemd/timesyncd.conf
         systemctl restart systemd-timesyncd
         timedatectl
+      #Set LED to solid blue after network interface comes up
+      echo '#!/bin/sh
+#This script turns off white LED and turns on blue LED when network interface comes up.
+echo rfkill0 > /sys/class/leds/blue/trigger
+echo none > /sys/class/leds/white/trigger' > /etc/network/if-up.d/0-led-blue.sh
+        chmod +x /etc/network/if-up.d/0-led-blue.sh
+        sh /etc/network/if-up.d/0-led-blue.sh
       #Option to run Device-Config.sh
       while : ; do
         read -p "$(echo '\033[0;106m'"\033[30mRun Device-Config (set static IP, hostname, harden SSH, etc.)? (y/n)\033[0m ")" yn
