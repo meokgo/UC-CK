fb79f06222641013153983d980bed9dfc6a6e7e3
meokgo <cimdieke@hotmail.com>
2024-10-21 16:41:51 -0500

    Device-Config.sh

--- Device-Config.sh
+++ Device-Config.sh
@@ -3,6 +3,25 @@
 #Download script: sudo wget https://raw.githubusercontent.com/meokgo/UC-CK/main/Device-Config.sh
 #Make script executable: sudo chmod +x Device-Config.sh
 #Run script: sudo ./Device-Config.sh
+
+setup_users ()
+{
+  #Prompt for which user to setup 2FA for
+  while : ; do
+    read  -p "$(echo '\033[0;106m'"\033[30mSetup 2FA for users? (y/n)\033[0m ")" yn
+    case $yn in
+      [yY] read -p "$(echo '\033[0;106m'"\033[30mEnter user name to setup 2FA:\033[0m ")" 2FA_User
+        if [ -z "2FA_User" ];
+        then
+          echo '\033[0;35m'"\033[1mNothing entered.\033[0m"
+        else
+          runuser -l $2FA_User -c 'google-authenticator -tdf -Q UTF8 -r 3 -R 30 -w 3'
+        fi
+      [nN]  echo '\033[0;35m'"\033[1mDone setting up 2FA users.\033[0m"
+      *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
+    esac
+  done
+}
 (
 echo "$(date): Script started." >> Device-Config.log
 #Check if script is run as root
@@ -217,24 +236,6 @@ fi" > /etc/profile.d/ssh-timeout.sh
   esac
 done
 #Option to enable 2FA
-setup_users ()
-{
-  #Prompt for which user to setup 2FA for
-  while : ; do
-    read  -p "$(echo '\033[0;106m'"\033[30mSetup 2FA for users? (y/n)\033[0m ")" yn
-    case $yn in
-      [yY] read -p "$(echo '\033[0;106m'"\033[30mEnter user name to setup 2FA:\033[0m ")" 2FA_User
-        if [ -z "2FA_User" ];
-        then
-          echo '\033[0;35m'"\033[1mNothing entered.\033[0m"
-        else
-          runuser -l $2FA_User -c 'google-authenticator -tdf -Q UTF8 -r 3 -R 30 -w 3'
-        fi
-      [nN]  echo '\033[0;35m'"\033[1mDone setting up 2FA users.\033[0m"
-      *) echo '\033[0;31m'"\033[1mInvalid response.\033[0m";;
-  esac
-done
-}
 while : ; do
   read  -p "$(echo '\033[0;106m'"\033[30mSetup Google Authenticator? (y/n)\033[0m ")" yn
   case $yn in
