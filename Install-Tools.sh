36db55f7c1124a526fc62fac75d6dc1ef54f31dd
meokgo <cimdieke@hotmail.com>
2024-10-24 14:56:43 -0500

    Install-Tools.sh

--- Install-Tools.sh
+++ Install-Tools.sh
@@ -40,11 +40,15 @@ splitw -v -p 50 -t ssh_tmux
 respawn-pane -t ssh_tmux:0.0 -k 'run-parts /etc/update-motd.d && bash'
 respawn-pane -t ssh_tmux:0.1 -k 'btop && bash'
 respawn-pane -t ssh_tmux:0.2 -k 'cmatrix && bash'
-#Select left pane
+select-pane -t 0.2 -T cmatrix
+select-pane -t 0.1 -T btop
+#Select left pane -T bash
 select-pane -t 0
 #Enable mouse
 set -g mouse on
 #Status line
+set pane-border-status top
+set -g pane-border-format '#[fg=black, bg=green] #{pane_index} #T'
 set -g status on
 set -g status-interval 1
 set -g status-justify centre
