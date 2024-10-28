3a73500c78a539f8d46831841a4e8227640a0773
meokgo <cimdieke@hotmail.com>
2024-10-28 16:30:45 -0500

    btop.conf.sh

--- btop.conf.sh
+++ btop.conf.sh
@@ -5,8 +5,8 @@
   sed -i 's|theme_background = True|theme_background = False|g' ~/.config/btop/btop.conf
   sed -i 's|show_boxes =|show_boxes = "net mem"|g' ~/.config/btop/btop.conf
   sed -i 's|rouded_corners = True|rouded_corners = False|g' ~/.config/btop/btop.conf
-  sed -i 's|graph_symbol_mem = "default"|graph_symbol_mem = "block" ~/.config/btop/btop.conf
-  sed -i 's|graph_symbol_net = "default"|graph_symbol_net = "braille" ~/.config/btop/btop.conf
+  sed -i 's|graph_symbol_mem = "default"|graph_symbol_mem = "block"|g' ~/.config/btop/btop.conf
+  sed -i 's|graph_symbol_net = "default"|graph_symbol_net = "braille"|g' ~/.config/btop/btop.conf
   sed -i 's|disk_filter =|disk_filter = "/ /srv"|g' ~/.config/btop/btop.conf
   sed -i 's|only_physical = True|only_physical = False|g' ~/.config/btop/btop.conf
   sed -i 's|use_fstab = True|use_fstab = False|g' ~/.config/btop/btop.conf
