010c7e2c8498428d1930de687f7a86a72d7723cd
meokgo <cimdieke@hotmail.com>
2024-10-28 16:26:54 -0500

    btop.conf.sh

---
+++ btop.conf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#This script creates btop config file for use in split screen tmux session
+  cp ~/.config/btop/btop.conf ~/.config/btop/btop.conf.bak
+  sed -i 's|color_theme "Default"|color_theme "TTY"|g' ~/.config/btop/btop.conf
+  sed -i 's|theme_background = True|theme_background = False|g' ~/.config/btop/btop.conf
+  sed -i 's|show_boxes =|show_boxes = "net mem"|g' ~/.config/btop/btop.conf
+  sed -i 's|rouded_corners = True|rouded_corners = False|g' ~/.config/btop/btop.conf
+  sed -i 's|graph_symbol_mem = "default"|graph_symbol_mem = "block" ~/.config/btop/btop.conf
+  sed -i 's|graph_symbol_net = "default"|graph_symbol_net = "braille" ~/.config/btop/btop.conf
+  sed -i 's|disk_filter =|disk_filter = "/ /srv"|g' ~/.config/btop/btop.conf
+  sed -i 's|only_physical = True|only_physical = False|g' ~/.config/btop/btop.conf
+  sed -i 's|use_fstab = True|use_fstab = False|g' ~/.config/btop/btop.conf
+  sed -i 's|show_battery = True|show_battery = False|g' ~/.config/btop/btop.conf
+  sed -i 's|net_iface = ""|net_iface = "eth0"|g' ~/.config/btop/btop.conf
