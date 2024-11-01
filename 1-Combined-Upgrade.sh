398cbf89b8b68374ce1b0e5b7e803845ccc087eb
meokgo <cimdieke@hotmail.com>
2024-11-01 13:34:32 -0500

    1-Combined-Upgrade.sh

--- 1-Combined-Upgrade.sh
+++ 1-Combined-Upgrade.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #This script will upgrade the factory default state OS from Debian Jessie to Debian Buster or Debian Buster to Debian Bullseye on UniFi Cloud Key Model: UC-CK
 #This script will disable or remove most UniFi packages, the device will no longer function as a Cloud Key for UniFi devices, but Emergency Recovery UI still works.
-#****It is highly recommended to factory reset the Cloud Key before running script the first time****
+#+----------------------------------------------------------------------------------------------------+
+#|****It is highly recommended to factory reset the Cloud Key before running script the first time****|
+#+----------------------------------------------------------------------------------------------------+
 #Factory reset Cloud Key: sudo ubnt-systool reset2defaults
 #Default SSH user: root
 #Default SSH password: ubnt
